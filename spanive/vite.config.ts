6abb48cdd4467aaaf544b686f3554c312fd732f6
David <davidbackk02@gmail.com>
2025-05-10 13:23:02 +0200

    try10

--- spanive/vite.config.ts
+++ spanive/vite.config.ts
@@ -27,6 +27,7 @@ export default defineConfig({
     },
     rollupOptions: {
       output: {
+        format: 'es',
         manualChunks: {
           vendor: ['react', 'react-dom'],
           router: ['react-router-dom']
